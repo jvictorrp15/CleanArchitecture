96c583f141357d52bdc1ef305efc77770c01df65
João Victor Ramalho Pereira <victorjudoca2009@hotmail.com>
2022-07-04 16:50:46 -0300

    first commit

---
+++ app/src/main/kotlin/com/fernandocejas/sample/features/movies/MovieDetailsFragment.kt
@@ -0,0 +1,115 @@
+/**
+ * Copyright (C) 2020 Fernando Cejas Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fernandocejas.sample.features.movies
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
+import com.fernandocejas.sample.R
+import com.fernandocejas.sample.core.exception.Failure
+import com.fernandocejas.sample.core.exception.Failure.NetworkConnection
+import com.fernandocejas.sample.core.exception.Failure.ServerError
+import com.fernandocejas.sample.core.extension.*
+import com.fernandocejas.sample.core.platform.BaseFragment
+import com.fernandocejas.sample.features.movies.MovieFailure.NonExistentMovie
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_movie_details.*
+import kotlinx.android.synthetic.main.toolbar.*
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MovieDetailsFragment : BaseFragment() {
+
+    companion object {
+        private const val PARAM_MOVIE = "param_movie"
+
+        fun forMovie(movie: MovieView?) = MovieDetailsFragment().apply {
+            arguments = bundleOf(PARAM_MOVIE to movie)
+        }
+    }
+
+    @Inject
+    lateinit var movieDetailsAnimator: MovieDetailsAnimator
+
+    private val movieDetailsViewModel by viewModels<MovieDetailsViewModel>()
+
+    override fun layoutId() = R.layout.fragment_movie_details
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.let { movieDetailsAnimator.postponeEnterTransition(it) }
+
+        with(movieDetailsViewModel) {
+            observe(movieDetails, ::renderMovieDetails)
+            failure(failure, ::handleFailure)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (firstTimeCreated(savedInstanceState)) {
+            movieDetailsViewModel.loadMovieDetails((arguments?.get(PARAM_MOVIE) as MovieView).id)
+        } else {
+            movieDetailsAnimator.scaleUpView(moviePlay)
+            movieDetailsAnimator.cancelTransition(moviePoster)
+            moviePoster.loadFromUrl((requireArguments()[PARAM_MOVIE] as MovieView).poster)
+        }
+    }
+
+    override fun onBackPressed() {
+        movieDetailsAnimator.fadeInvisible(scrollView, movieDetails)
+        if (moviePlay.isVisible())
+            movieDetailsAnimator.scaleDownView(moviePlay)
+        else
+            movieDetailsAnimator.cancelTransition(moviePoster)
+    }
+
+    private fun renderMovieDetails(movie: MovieDetailsView?) {
+        movie?.let {
+            with(movie) {
+                activity?.let {
+                    moviePoster.loadUrlAndPostponeEnterTransition(poster, it)
+                    it.toolbar.title = title
+                }
+                movieSummary.text = summary
+                movieCast.text = cast
+                movieDirector.text = director
+                movieYear.text = year.toString()
+                moviePlay.setOnClickListener { movieDetailsViewModel.playMovie(trailer) }
+            }
+        }
+        movieDetailsAnimator.fadeVisible(scrollView, movieDetails)
+        movieDetailsAnimator.scaleUpView(moviePlay)
+    }
+
+    private fun handleFailure(failure: Failure?) {
+        when (failure) {
+            is NetworkConnection -> {
+                notify(R.string.failure_network_connection); close()
+            }
+            is ServerError -> {
+                notify(R.string.failure_server_error); close()
+            }
+            is NonExistentMovie -> {
+                notify(R.string.failure_movie_non_existent); close()
+            }
+            else -> {
+                notify(R.string.failure_server_error); close()
+            }
+        }
+    }
+}
